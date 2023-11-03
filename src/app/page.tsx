3b2715424a232c7b88d0b5acdd60c49a595430bb
Alessandro Reis <alessandrodreis23@gmail.com>
2023-11-03 17:15:31 -0300

    base structure landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
-			<p>Hello Git Center App</p>
+		<main className='flex min-h-screen flex-col p-24'>
+			{/* header fixed (?) */}
+			<div className='flex justify-between'>
+				{/* logo / nome do site */}
+
+				{/* botao de login com github */}
+			</div>
+
+			{/* page */}
+			<div className='flex'>
+				{/* Application information */}
+
+				{/* Login button */}
+			</div>
 		</main>
 	)
 }
