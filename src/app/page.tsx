fe22ed51612c96213b4852ce7c6714c2d224af43
Alessandro Reis <alessandrodreis23@gmail.com>
2023-11-06 16:59:20 -0300

    create landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,35 @@
+import { GithubButton } from '@/components/GithubButton'
+import { ThemeButton } from '@/components/ThemeButton'
+import { GithubIcon } from 'lucide-react'
+
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col p-24'>
-			{/* header fixed (?) */}
-			<div className='flex justify-between'>
-				{/* logo / nome do site */}
+		<main className='flex min-h-screen flex-col'>
+			{/* Icon will act as background image for the page */}
+			<GithubIcon className='absolute w-full h-full opacity-25 -z-10' />
 
-				{/* botao de login com github */}
+			{/* header */}
+			<div className='flex justify-between items-center px-10 py-5'>
+				<h2 className='text-lg font-bold'>Git Center</h2>
+
+				<div className='flex gap-3'>
+					<ThemeButton />
+					<GithubButton />
+				</div>
 			</div>
 
 			{/* page */}
-			<div className='flex'>
-				{/* Application information */}
+			<div className='flex-1 py-10 flex flex-col gap-36 mx-auto'>
+				<div className='flex flex-col gap-3'>
+					<h1 className='font-bold text-2xl'>
+						Manage all your Github Repositories
+					</h1>
+					<p className='text-xl'>The app to center all your work</p>
+				</div>
 
-				{/* Login button */}
+				<div className='flex items-center justify-center'>
+					<GithubButton />
+				</div>
 			</div>
 		</main>
 	)
