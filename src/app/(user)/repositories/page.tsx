8e1cb0decaecd2244fa041a1f2d5ea155f0c07a8
Alessandro Reis <alessandrodreis23@gmail.com>
2023-11-08 13:34:42 -0300

    created Repository component for list of all repositories from user (using octocat for test)

--- src/app/(user)/repositories/page.tsx
+++ src/app/(user)/repositories/page.tsx
@@ -1,3 +1,24 @@
-export default function RepositoriesPage() {
-	return <p>Repositories</p>
+import { Repository } from '@/components/Repository'
+import { getRepositories } from '@/utils/fetch'
+
+export default async function RepositoriesPage() {
+	const repositories = await getRepositories('octocat')
+
+	return (
+		<div className='w-full flex flex-col gap-5'>
+			<div className='flex flex-col gap-3'>
+				<h1 className='font-bold'>Repositories in this account</h1>
+				<div className='w-full h-px bg-zinc-500' />
+			</div>
+
+			<div className='flex flex-col gap-1'>
+				{repositories.map((repository) => (
+					<Repository
+						key={repository.id}
+						repository={repository}
+					/>
+				))}
+			</div>
+		</div>
+	)
 }
---
+++ src/components/Repository.tsx
@@ -0,0 +1,43 @@
+import { GithubRepoResponse } from '@/@types/github'
+import { getRepositoryCommits } from '@/utils/fetch'
+import { BookMarkedIcon } from 'lucide-react'
+import Link from 'next/link'
+
+export async function Repository({
+	repository,
+}: {
+	repository: GithubRepoResponse
+}) {
+	const commits = await getRepositoryCommits('octocat', repository.name)
+	const lastCommit = commits.at(0)
+	const commitMessage = lastCommit?.commit.message
+
+	return (
+		<Link
+			href={`/repositories/${repository.id}`}
+			className='flex items-center justify-between border border-zinc-400 py-2 px-4 mx-5'>
+			<div className='flex gap-3 items-center'>
+				<BookMarkedIcon />
+				<div className='flex flex-col w-96'>
+					<span className='font-semibold'>{repository.name}</span>
+					<span
+						className='font-light text-zinc-400 truncate'
+						title={repository.description}>
+						{repository.description}
+					</span>
+				</div>
+			</div>
+
+			<span className='font-light text-zinc-400 text-start flex-1 px-10'>
+				{commitMessage || 'No commit found'}
+			</span>
+
+			<div className='flex flex-col'>
+				<span className='italic'>Last updated: {repository.pushed_at}</span>
+				<span className='font-light italic text-zinc-400'>
+					Language: {repository.language}
+				</span>
+			</div>
+		</Link>
+	)
+}
