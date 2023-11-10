d09dbdc6c38380d0ffaab5e71369dbf1705e1407
Alessandro Reis <alessandrodreis23@gmail.com>
2023-11-10 16:57:05 -0300

    create followscard component

---
+++ src/components/FollowsCard.tsx
@@ -0,0 +1,34 @@
+import { UserResponse } from '@/@types/github'
+import Avatar from '@/components/Avatar'
+
+export function FollowsCard({
+	users,
+	type,
+}: {
+	users: UserResponse[]
+	type: 'followers' | 'following'
+}) {
+	return (
+		<div className='flex flex-col gap-3 items-center bg-zinc-800 rounded-md p-3 flex-1'>
+			<span className='self-start text-lg font-semibold capitalize'>
+				{type}{' '}
+				<span className='font-normal text-base italic lowercase'>
+					(shows max 10 {type})
+				</span>
+			</span>
+			<div className='flex gap-1'>
+				{users.length > 0 ? (
+					users.slice(0, 10).map((user) => (
+						<Avatar
+							key={user.login}
+							user={user}
+							className='h-10 w-10 border'
+						/>
+					))
+				) : (
+					<span className='italic text-zinc-500'>No {type} found</span>
+				)}
+			</div>
+		</div>
+	)
+}
