08874024c0ebf628e44b73308c9b1c7be4d29805
Alessandro Reis <alessandrodreis23@gmail.com>
2023-11-10 14:50:27 -0300

    added new background colors to all cards on header

--- src/app/(user)/dashboard/page.tsx
+++ src/app/(user)/dashboard/page.tsx
@@ -18,98 +18,104 @@ export default async function DashboardPage() {
 	const following = await getUserFollowing('octocat')
 
 	return (
-		<div className='w-full flex flex-col gap-5 py-5 px-3'>
-			<div className='flex w-full justify-between items-center'>
-				<div className='flex gap-2 items-center'>
-					<Avatar user={user} />
+		<div className='w-full flex flex-col gap-5 px-1'>
+			{/* Header */}
+			<div className='flex flex-col bg-zinc-900 p-3 gap-3 rounded-md'>
+				<div className='flex w-full justify-between items-center'>
+					{/* User Details */}
+					<div className='flex gap-2 items-center bg-zinc-800 rounded-md p-2'>
+						<Avatar user={user} />
 
-					<div className='flex flex-col'>
-						<h2 className='text-xl font-extrabold'>
-							Hello, <span className='text-violet-500'>{user.name}</span>
-						</h2>
-						<span className='text-zinc-300 text-lg'>@{user.login}</span>
+						<div className='flex flex-col'>
+							<h2 className='text-xl font-extrabold'>
+								Hello, <span className='text-violet-500'>{user.name}</span>
+							</h2>
+							<span className='text-zinc-300 text-lg'>@{user.login}</span>
+						</div>
 					</div>
-				</div>
 
-				<div className='grid grid-cols-2 gap-2'>
-					<div
-						className='flex gap-1'
-						title='Company'>
-						<BuildingIcon />
-						<span className='text-zinc-300'>
-							{user.company || 'Not registered'}
-						</span>
-					</div>
-					<div
-						className='flex gap-1'
-						title='Email'>
-						<MailIcon />
-						<span className='text-zinc-300'>
-							{user.email || 'Not registered'}
-						</span>
-					</div>
-					<div
-						className='flex gap-1'
-						title='Location'>
-						<MapPinIcon />
-						<span className='text-zinc-300'>
-							{user.location || 'Not registered'}
-						</span>
-					</div>
-					<div
-						className='flex gap-1'
-						title='Blog'>
-						<LinkIcon />
-						{user.blog ? (
-							<Link
-								href={user.blog}
-								target='_blank'
-								className='text-zinc-300'>
-								{user.blog}
-							</Link>
-						) : (
+					{/* User Contact */}
+					<div className='grid grid-cols-2 gap-2 bg-zinc-800 rounded-md p-2'>
+						<div
+							className='flex gap-1'
+							title='Company'>
+							<BuildingIcon />
 							<span className='text-zinc-300'>
-								{user.blog || 'Not registered'}
+								{user.company || 'Not registered'}
 							</span>
-						)}
+						</div>
+						<div
+							className='flex gap-1'
+							title='Email'>
+							<MailIcon />
+							<span className='text-zinc-300'>
+								{user.email || 'Not registered'}
+							</span>
+						</div>
+						<div
+							className='flex gap-1'
+							title='Location'>
+							<MapPinIcon />
+							<span className='text-zinc-300'>
+								{user.location || 'Not registered'}
+							</span>
+						</div>
+						<div
+							className='flex gap-1'
+							title='Blog'>
+							<LinkIcon />
+							{user.blog ? (
+								<Link
+									href={user.blog}
+									target='_blank'
+									className='text-zinc-300'>
+									{user.blog}
+								</Link>
+							) : (
+								<span className='text-zinc-300'>
+									{user.blog || 'Not registered'}
+								</span>
+							)}
+						</div>
 					</div>
+
+					{/* User Git Stats */}
+					<div className='grid grid-cols-2 gap-2 bg-zinc-800 rounded-md p-2'>
+						<div className='flex gap-1'>
+							<BookMarkedIcon />
+							<span className='text-zinc-300'>
+								{user.public_repos} public repos
+							</span>
+						</div>
+						<div className='flex gap-1'>
+							<Users2Icon />
+							<span className='text-zinc-300'>{user.followers} followers</span>
+						</div>
+						<div className='flex gap-1'>
+							<SquareCodeIcon />
+							<span className='text-zinc-300'>
+								{user.public_gists} pubic gists
+							</span>
+						</div>
+						<div className='flex gap-1'>
+							<Users2Icon />
+							<span className='text-zinc-300'>{user.following} following</span>
+						</div>
+					</div>
+
+					<Link
+						href={user.html_url}
+						target='_blank'
+						className='flex items-center gap-1 border px-2 py-1 rounded-md'>
+						<GithubIcon />
+						Go to Github
+					</Link>
 				</div>
 
-				<div className='grid grid-cols-2 gap-2'>
-					<div className='flex gap-1'>
-						<BookMarkedIcon />
-						<span className='text-zinc-300'>
-							{user.public_repos} public repos
-						</span>
-					</div>
-					<div className='flex gap-1'>
-						<Users2Icon />
-						<span className='text-zinc-300'>{user.followers} followers</span>
-					</div>
-					<div className='flex gap-1'>
-						<SquareCodeIcon />
-						<span className='text-zinc-300'>
-							{user.public_gists} pubic gists
-						</span>
-					</div>
-					<div className='flex gap-1'>
-						<Users2Icon />
-						<span className='text-zinc-300'>{user.following} following</span>
-					</div>
-				</div>
-
-				<Link
-					href={user.html_url}
-					target='_blank'
-					className='flex items-center gap-1 border px-2 py-1 rounded-md'>
-					<GithubIcon />
-					Go to Github
-				</Link>
+				<span className='text-zinc-300 italic w-full'>
+					{user.bio || "You didn't wrote nothing in your Github profile bio"}
+				</span>
 			</div>
-
-			<span className='text-zinc-300 italic w-full'>
-				{user.bio || "You didn't wrote nothing in your Github profile bio"}
-			</span>
 
 			<div className='w-full h-px bg-zinc-500' />
 
