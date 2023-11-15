4d21c2e00a4dcf26a01e0878328a8a930dc396a2
Alessandro Reis <alessandrodreis23@gmail.com>
2023-11-15 11:35:47 -0300

    remove nextauth User type extension

--- src/@types/next-auth.d.ts
+++ src/@types/next-auth.d.ts
@@ -1,31 +1,21 @@
-import {
-	Profile as DefaultProfile,
-	DefaultSession,
-	User as DefaultUser,
-} from 'next-auth'
+import { Profile as DefaultProfile, DefaultSession } from 'next-auth'
+import { UserResponse } from './github'
 
 declare module 'next-auth' {
 	interface Session {
 		user: {
 			access_token?: string
-			profile?: Profile
+			profile: Profile
 		} & DefaultSession['user']
 	}
 
-	interface Profile extends DefaultProfile {
-		login: string
-	}
-
-	interface User extends DefaultUser {
-		profile?: Profile
-		access_token?: string
-	}
+	interface Profile extends DefaultProfile, UserResponse {}
 }
 
 declare module '@auth/core/jwt' {
 	/** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
 	interface JWT {
 		access_token?: string
-		profile?: Profile
+		profile: Profile
 	}
 }
