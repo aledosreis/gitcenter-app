97c6e36430f5a4a3febce89a565dafa59870bf1e
Alessandro Reis <alessandrodreis23@gmail.com>
2023-11-15 12:04:32 -0300

    authenticating requests

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -9,18 +9,34 @@ const BASE_API_URL = 'https://api.github.com/'
 /**
  * Fetch data from Github user
  * @param user username from a Github user
+ * @param access_token token for authorization request
  */
-export async function getUserData(user: string): Promise<UserResponse> {
-	const response = await fetch(`${BASE_API_URL}users/${user}`)
+export async function getUserData(
+	user: string,
+	access_token: string
+): Promise<UserResponse> {
+	const response = await fetch(`${BASE_API_URL}users/${user}`, {
+		headers: {
+			Authorization: `Bearer ${access_token}`,
+		},
+	})
 	return response.json()
 }
 
 /**
  * Fech all repositories owned by a Github user
  * @param user  username from a Github User
+ * @param access_token token for authorization request
  */
-export async function getRepositories(user: string): Promise<RepoResponse[]> {
-	const response = await fetch(`${BASE_API_URL}users/${user}/repos`)
+export async function getRepositories(
+	user: string,
+	access_token: string
+): Promise<RepoResponse[]> {
+	const response = await fetch(`${BASE_API_URL}users/${user}/repos`, {
+		headers: {
+			Authorization: `Bearer ${access_token}`,
+		},
+	})
 	return response.json()
 }
 
@@ -28,12 +44,18 @@ export async function getRepositories(user: string): Promise<RepoResponse[]> {
  * Fetch data from a specific repository
  * @param owner username from the repository's owner
  * @param repository name of the repository
+ * @param access_token token for authorization request
  */
 export async function getRepositoryData(
 	owner: string,
-	repository: string
+	repository: string,
+	access_token: string
 ): Promise<RepoResponse> {
-	const response = await fetch(`${BASE_API_URL}repos/${owner}/${repository}`)
+	const response = await fetch(`${BASE_API_URL}repos/${owner}/${repository}`, {
+		headers: {
+			Authorization: `Bearer ${access_token}`,
+		},
+	})
 	return response.json()
 }
 
@@ -41,13 +63,20 @@ export async function getRepositoryData(
  * Fetch all commits from a repository
  * @param user username from the repository's owner
  * @param repository name of the repository
+ * @param access_token token for authorization request
  */
 export async function getRepositoryCommits(
 	user: string,
-	repository: string
+	repository: string,
+	access_token: string
 ): Promise<RepositoryCommitResponse[]> {
 	const response = await fetch(
-		`${BASE_API_URL}repos/${user}/${repository}/commits`
+		`${BASE_API_URL}repos/${user}/${repository}/commits`,
+		{
+			headers: {
+				Authorization: `Bearer ${access_token}`,
+			},
+		}
 	)
 
 	return response.json()
@@ -56,9 +85,17 @@ export async function getRepositoryCommits(
 /**
  * Fetch followers from Github user
  * @param user username from a Github user
+ * @param access_token token for authorization request
  */
-export async function getUserFollowers(user: string): Promise<UserResponse[]> {
-	const response = await fetch(`${BASE_API_URL}users/${user}/followers`)
+export async function getUserFollowers(
+	user: string,
+	access_token: string
+): Promise<UserResponse[]> {
+	const response = await fetch(`${BASE_API_URL}users/${user}/followers`, {
+		headers: {
+			Authorization: `Bearer ${access_token}`,
+		},
+	})
 
 	return response.json()
 }
@@ -66,9 +103,17 @@ export async function getUserFollowers(user: string): Promise<UserResponse[]> {
 /**
  * Fetch following from Github user
  * @param user username from a Github user
+ * @param access_token token for authorization request
  */
-export async function getUserFollowing(user: string): Promise<UserResponse[]> {
-	const response = await fetch(`${BASE_API_URL}users/${user}/following`)
+export async function getUserFollowing(
+	user: string,
+	access_token: string
+): Promise<UserResponse[]> {
+	const response = await fetch(`${BASE_API_URL}users/${user}/following`, {
+		headers: {
+			Authorization: `Bearer ${access_token}`,
+		},
+	})
 
 	return response.json()
 }
