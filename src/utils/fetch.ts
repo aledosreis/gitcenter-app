c4ef19e710dfa6ee7a3163745b8545f655e45574
Alessandro Reis <alessandrodreis23@gmail.com>
2023-11-16 10:10:10 -0300

    changed functions to build url and pass this url to a new function for request with token

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -3,56 +3,54 @@ import {
 	RepositoryCommitResponse,
 	UserResponse,
 } from '@/@types/github'
+import { auth } from '@/auth'
 
 const BASE_API_URL = 'https://api.github.com/'
 
 /**
- * Fetch data from Github user
- * @param user username from a Github user
- * @param access_token token for authorization request
+ * Fetch data from provided url
+ * @param url URL of request
  */
-export async function getUserData(
-	user: string,
-	access_token: string
-): Promise<UserResponse> {
-	const response = await fetch(`${BASE_API_URL}users/${user}`, {
+async function fetchData(url: string) {
+	const session = await auth()
+	if (!session) return Response.json({ message: 'Not authenticated' })
+
+	const response = await fetch(url, {
 		headers: {
-			Authorization: `Bearer ${access_token}`,
+			Authorization: `Bearer ${session.user.access_token}`,
 		},
 	})
+
+	return response
+}
+
+/**
+ * Fetch data from Github user
+ * @param user username from a Github user
+ */
+export async function getUserData(user: string): Promise<UserResponse> {
+	const url = `${BASE_API_URL}users/${user}`
+	const response = await fetchData(url)
 	return response.json()
 }
 
 /**
  * Fech all repositories owned by a Github user
  * @param user  username from a Github User
- * @param access_token token for authorization request
  * @variation getCurrentUserRepositories for current user repositories
  */
-export async function getRepositories(
-	user: string,
-	access_token: string
-): Promise<RepoResponse[]> {
-	const response = await fetch(`${BASE_API_URL}users/${user}/repos`, {
-		headers: {
-			Authorization: `Bearer ${access_token}`,
-		},
-	})
+export async function getRepositories(user: string): Promise<RepoResponse[]> {
+	const url = `${BASE_API_URL}users/${user}/repos`
+	const response = await fetchData(url)
 	return response.json()
 }
 
 /**
  * Fech all repositories owned by a Github user
- * @param access_token token for authorization request
  */
-export async function getCurrentUserRepositories(
-	access_token: string
-): Promise<RepoResponse[]> {
-	const response = await fetch(`${BASE_API_URL}user/repos`, {
-		headers: {
-			Authorization: `Bearer ${access_token}`,
-		},
-	})
+export async function getCurrentUserRepositories(): Promise<RepoResponse[]> {
+	const url = `${BASE_API_URL}user/repos`
+	const response = await fetchData(url)
 	return response.json()
 }
 
@@ -60,18 +58,13 @@ export async function getCurrentUserRepositories(
  * Fetch data from a specific repository
  * @param owner username from the repository's owner
  * @param repository name of the repository
- * @param access_token token for authorization request
  */
 export async function getRepositoryData(
 	owner: string,
-	repository: string,
-	access_token: string
+	repository: string
 ): Promise<RepoResponse> {
-	const response = await fetch(`${BASE_API_URL}repos/${owner}/${repository}`, {
-		headers: {
-			Authorization: `Bearer ${access_token}`,
-		},
-	})
+	const url = `${BASE_API_URL}repos/${owner}/${repository}`
+	const response = await fetchData(url)
 	return response.json()
 }
 
@@ -79,91 +72,63 @@ export async function getRepositoryData(
  * Fetch all commits from a repository
  * @param user username from the repository's owner
  * @param repository name of the repository
- * @param access_token token for authorization request
  */
 export async function getRepositoryCommits(
 	user: string,
-	repository: string,
-	access_token: string
+	repository: string
 ): Promise<RepositoryCommitResponse[]> {
-	const response = await fetch(
-		`${BASE_API_URL}repos/${user}/${repository}/commits`,
-		{
-			headers: {
-				Authorization: `Bearer ${access_token}`,
-			},
-		}
-	)
-
+	const url = `${BASE_API_URL}repos/${user}/${repository}/commits`
+	const response = await fetchData(url)
 	return response.json()
 }
 
 /**
  * Fetch followers from Github user
  * @param user username from a Github user
- * @param access_token token for authorization request
  * @variation getCurrentUserFollowers for current user followers
  */
-export async function getUserFollowers(
-	user: string,
-	access_token: string
-): Promise<UserResponse[]> {
-	const response = await fetch(`${BASE_API_URL}users/${user}/followers`, {
-		headers: {
-			Authorization: `Bearer ${access_token}`,
-		},
-	})
-
+export async function getUserFollowers(user: string): Promise<UserResponse[]> {
+	const url = `${BASE_API_URL}users/${user}/followers`
+	const response = await fetchData(url)
 	return response.json()
 }
 
 /**
  * Fetch followers from Github user
- * @param access_token token for authorization request
  */
-export async function getCurrentUserFollowers(
-	access_token: string
-): Promise<UserResponse[]> {
-	const response = await fetch(`${BASE_API_URL}user/followers`, {
-		headers: {
-			Authorization: `Bearer ${access_token}`,
-		},
-	})
-
+export async function getCurrentUserFollowers(): Promise<UserResponse[]> {
+	const url = `${BASE_API_URL}user/followers`
+	const response = await fetchData(url)
 	return response.json()
 }
 
 /**
  * Fetch following from Github user
  * @param user username from a Github user
- * @param access_token token for authorization request
  * @variation getCurrentUserFollowing for current user following
  */
-export async function getUserFollowing(
-	user: string,
-	access_token: string
-): Promise<UserResponse[]> {
-	const response = await fetch(`${BASE_API_URL}users/${user}/following`, {
-		headers: {
-			Authorization: `Bearer ${access_token}`,
-		},
-	})
-
+export async function getUserFollowing(user: string): Promise<UserResponse[]> {
+	const url = `${BASE_API_URL}users/${user}/following`
+	const response = await fetchData(url)
 	return response.json()
 }
 
 /**
  * Fetch following from Github user
- * @param access_token token for authorization request
  */
-export async function getCurrentUserFollowing(
-	access_token: string
-): Promise<UserResponse[]> {
-	const response = await fetch(`${BASE_API_URL}user/following`, {
-		headers: {
-			Authorization: `Bearer ${access_token}`,
-		},
-	})
+export async function getCurrentUserFollowing(): Promise<UserResponse[]> {
+	const url = `${BASE_API_URL}user/following`
+	const response = await fetchData(url)
+	return response.json()
+}
 
+/**
+ * Fetch repository's README.md file
+ * @param owner owner of repository
+ * @param repository name of repository
+ */
+export async function getRepositoryReadme(owner: string, repository: string) {
+	const url = `${BASE_API_URL}repos/${owner}/${repository}/readme`
+	const response = await fetchData(url)
 	return response.json()
 }
